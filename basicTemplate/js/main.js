03ddc075c5fc8415beca6813a0938a0e5b306617
armst209 <armst209@gmail.com>
2020-08-27 17:38:44 -0400

    added basicTemplate folder

---
+++ basicTemplate/js/main.js
@@ -0,0 +1,11 @@
+let game;
+window.onload = function () {
+  let config = {
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640,
+    parent: "phaser-game",
+    scene: [SceneMain],
+  };
+  game = new Phaser.Game(config);
+};
---
+++ basicTemplate/js/sceneMain.js
@@ -0,0 +1,23 @@
+class SceneMain extends Phaser.Scene {
+    //As soon as the scene loads the constructor starts
+    constructor() {
+        super('SceneMain');
+    }
+    preload()
+    {
+    	//load images or sounds
+    }
+    create() {
+
+        //defining our objects
+        console.log("Ready!");
+    }
+    update() {
+
+        //constant running loop i.e collision
+    }
+    customFunctions(){
+
+        //our own functions
+    }
+}

0f97022633ffc1950ca02a6a19f205025eac50db
armst209 <armst209@gmail.com>
2020-09-07 18:56:15 -0400

    attempting to fix js for landing page

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-
+ container: any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+

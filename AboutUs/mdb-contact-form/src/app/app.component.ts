3ebc7bddca400ea10f625c071f44cd8e06528a5c
Amber Dostert <amber.dostert@gmail.com>
2020-09-15 18:54:41 -0400

    chuckin away at the about us...kill me

---
+++ AboutUs/mdb-contact-form/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { ConnectionService } from './connection.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+
+contactForm: FormGroup;
+disabledSubmitButton: boolean = true;
+optionsSelect: Array<any>;
+
+  @HostListener('input') oninput() {
+
+  if (this.contactForm.valid) {
+    this.disabledSubmitButton = false;
+    }
+  }
+
+  constructor(private fb: FormBuilder, private connectionService: ConnectionService) {
+
+  this.contactForm = fb.group({
+    'contactFormName': ['', Validators.required],
+    'contactFormEmail': ['', Validators.compose([Validators.required, Validators.email])],
+    'contactFormSubjects': ['', Validators.required],
+    'contactFormMessage': ['', Validators.required],
+    'contactFormCopy': [''],
+    });
+  }
+
+  onSubmit() {
+    this.connectionService.sendMessage(this.contactForm.value).subscribe(() => {
+      alert('Your message has been sent.');
+      this.contactForm.reset();
+      this.disabledSubmitButton = true;
+    }, error => {
+      console.log('Error', error);
+    });
+  }
+
+  }
